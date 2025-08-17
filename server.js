60e7fd12b2e4b97036ab7da4ee4342fe8e0f87c3
Rounit20 <rounitsinha11@gmail.com>
2025-08-17 17:16:46 +0530

    fix: correct nodemailer createTransport method name

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ const upload = multer({
 let transporter = null;
 
 if (process.env.EMAIL_USER && process.env.EMAIL_APP_PASSWORD) {
-  transporter = nodemailer.createTransporter({
+  transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER,
