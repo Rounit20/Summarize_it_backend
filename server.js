aa784eecc4f6c3508c40cde5db964f1b7bd95241
Rounit20 <rounitsinha11@gmail.com>
2025-08-17 17:01:20 +0530

    Added new endpoint for front end

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (!fs.existsSync(uploadsDir)) {
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:3000', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:3000', 'http://127.0.0.1:5173','https://summarizer-6abe4.web.app/'],
   credentials: true
 }));
 app.use(express.json({ limit: '50mb' }));
